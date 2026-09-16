a21a3266ff18099a82f022149be136d126cd746a
Antonio Carlos <antonio_carlostads@hotmail.com>
2026-09-16 11:46:39 -0300

    chore: set up initial project structure

---
+++ src/main/java/br/com/prothera/teste_Iniflex/TesteIniflexApplication.java
@@ -0,0 +1,13 @@
+package br.com.prothera.teste_Iniflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteIniflexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteIniflexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/prothera/teste_Iniflex/TesteIniflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.prothera.teste_Iniflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteIniflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
